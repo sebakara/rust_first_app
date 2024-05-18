3ef132172a99c17b2771fea81e8aa319422feb0f
sebakara <maicseba@gmail.com>
2024-05-19 00:22:40 +0200

    with console input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
    let x = 4;
     println!("the value of x is: {}", x);
@@ -13,4 +15,20 @@ fn main() {
     // tupple
     let tup:(i32, bool, char) = (34, true, 'h');
     println!("let us see tupple: {}", tup.2);
+
+    // use array
+    let mut arr = [1,3,2,4,5];
+
+    arr[4] = 7;
+    println!("{}", arr[4]);
+
+    let y = 5;
+    let p = y;
+
+    println!("{},{}", y, p);
+
+    // inputs
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read line");
+    println!("this input has: {}", input);
 }
